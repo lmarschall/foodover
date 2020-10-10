b94f51ec09474e294170d7cd1d9f1443fcbc4315
Lukas Marschall <lmarschall.com@gmail.com>
2020-10-11 00:44:45 +0200

    added barcode detection

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
+var path = require('path');
 
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "static")));
+// app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 
-app.get('/', (req,res) => {
-  res.send("Hello");
-});
+// app.get('/', (req,res) => {
+//   res.send("Hello");
+// });
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
