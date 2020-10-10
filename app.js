1e20bfd6ab0be3b0d69b46fa9ab2eedaf3521278
Lukas Marschall <lmarschall.com@gmail.com>
2020-10-10 23:43:41 +0200

    created static files and views

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "static")));
+
 app.get('/', (req,res) => {
   res.send("Hello");
 });
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+  });
 
 app.listen(process.env.port || 3000);
 console.log('Web Server is listening at port '+ (process.env.port || 3000));
