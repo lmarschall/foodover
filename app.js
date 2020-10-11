bc0efb03fa9ce7136e00efff3878195bee6fd98c
Lukas Marschall <lmarschall.com@gmail.com>
2020-10-11 22:06:07 +0200

    fixing port error heroku deploy

--- app.js
+++ app.js
@@ -43,5 +43,5 @@ app.get("/api/product", (req, res) => {
   res.send(req.query.code)
 });
 
-app.listen(process.env.port || 3000);
-console.log('Web Server is listening at port '+ (process.env.port || 3000));
+app.listen(process.env.PORT || 3000);
+console.log('Web Server is listening at port '+ (process.env.PORT || 3000));
