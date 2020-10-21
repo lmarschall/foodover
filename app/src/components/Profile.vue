6b7e3e87f502f6fc35fef48be5fd6d250c3005d4
Lukas Marschall <lmarschall.com@gmail.com>
2020-10-21 20:53:26 +0200

    started implementing webcredentials

--- app/src/components/Profile.vue
+++ app/src/components/Profile.vue
@@ -2,6 +2,7 @@
     <div id="profileframe">
 
         <Bar v-bind:page="1" />
+        <input type="button" v-on:click="login">
         <Intolerances/>
 
 
@@ -32,9 +33,40 @@
 
             },
 
-            updateIntolerances: function()
+            login: async function()
             {
-                
+                var userIdBuffer;
+                var challengeBuffer;
+
+                const create_options = {
+                    publicKey: {
+                        rp: { name: "example.com" },
+                        user: {
+                            name: "john.appleseed@example.com",
+                            id: userIdBuffer,
+                            displayName: "John Appleseed"
+                        },
+                        pubKeyCredParams: [ { type: "public-key", alg: -7 } ],
+                        challenge: challengeBuffer,
+                        authenticatorSelection: { authenticatorAttachment: "platform" }
+                    }
+                };
+
+                const publicKeyCredential = await navigator.credentials.create(create_options);
+
+                console.log(publicKeyCredential)
+
+                // const get_options = {
+                //     publicKey: {
+                //         challenge: challengeBuffer,
+                //         allowCredentials: [
+                //             { type: "public-key", id: credentialIdBuffer1, transports: ["internal"] },
+                //             // ... more Credential IDs can be supplied.
+                //         ]
+                //     }
+                // };
+
+                // const publicKeyCredential = await navigator.credentials.get(get_options);
             }
         }
     }
