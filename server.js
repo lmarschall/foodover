694e2b54d159ebf494ef3462ea3a70987d8871a2
lmarschall <lmarschall.com@gmail.com>
2022-01-26 20:55:08 +0100

    added cors headers

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const port = process.env.PORT || 80;
 
 const history = require('connect-history-api-fallback');
 
+var cors = require('cors');
 app = express();
 
+app.use(cors());
+app.use(compression()); //COMPRESSION
 app.use(history());
 app.use(express.static(path.join(__dirname, "/app/dist")));
 app.use('/api', api);
-app.use(compression()); //COMPRESSION
 
 app.listen(port);
