06f8352c547ec933aae3ecb7440049086e21ab89
lmarschall <lmarschall.com@gmail.com>
2022-02-01 01:18:34 +0100

    "fixing cors issues"

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const api = require('./api');
 const webauthn = require('./webauthn');
 
 const port = process.env.PORT || 80;
-const medcastUrlRegExp = /(http(s)?:\/\/)(.foodover.app)|(localhost)/;
-const corsOptions = {
-    origin: medcastUrlRegExp
-};
+// const medcastUrlRegExp = /(http(s)?:\/\/)(.foodover.app)|(localhost)/;
+// const corsOptions = {
+//     origin: medcastUrlRegExp
+// };
 
 console.log('server is starting on port ' + port)
 
 app = express();
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(cors());
 app.use(compression()); //COMPRESSION
 app.use(bodyParser.json());
